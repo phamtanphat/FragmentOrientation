13bb28aa00a4b12f002f84eaebd0259ccc7a3801
thuviengithub0208 <khoaphp@yahoo.com>
2018-04-09 18:19:27 +0700

    "Update"

--- app/src/main/java/com/ptp/phamtanphat/fragmentorientation/Fragment_List.java
+++ app/src/main/java/com/ptp/phamtanphat/fragmentorientation/Fragment_List.java
@@ -5,16 +5,43 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 /**
  * Created by KhoaPhamPC on 9/4/2018.
  */
 
 public class Fragment_List extends ListFragment {
+
+
+    ArrayList<Nhanvien> mangnhanvien;
+    NhanvienAdapter nhanvienAdapter;
     View view;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.fragment_list,container,false);
+        mangnhanvien = new ArrayList<>();
+        mangnhanvien.add(new Nhanvien("Nguyen Van A","Quan 1","1990"));
+        mangnhanvien.add(new Nhanvien("Nguyen Van B","Quan 2","1991"));
+        mangnhanvien.add(new Nhanvien("Nguyen Van C","Quan 3","1992"));
+        mangnhanvien.add(new Nhanvien("Nguyen Van D","Quan 4","1993"));
+        mangnhanvien.add(new Nhanvien("Nguyen Van E","Quan 5","1994"));
+        mangnhanvien.add(new Nhanvien("Nguyen Van F","Quan 6","1995"));
+        mangnhanvien.add(new Nhanvien("Nguyen Van G","Quan 7","1996"));
+        mangnhanvien.add(new Nhanvien("Nguyen Van H","Quan 8","1997"));
+
+        nhanvienAdapter = new NhanvienAdapter(getActivity(),android.R.layout.simple_list_item_1,mangnhanvien);
+        setListAdapter(nhanvienAdapter);
+
         return view;
+    }
+
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        Toast.makeText(getActivity(), mangnhanvien.get(position).getTen(), Toast.LENGTH_SHORT).show();
+        super.onListItemClick(l, v, position, id);
     }
 }
--- app/src/main/java/com/ptp/phamtanphat/fragmentorientation/NhanvienAdapter.java
+++ app/src/main/java/com/ptp/phamtanphat/fragmentorientation/NhanvienAdapter.java
@@ -2,7 +2,13 @@ package com.ptp.phamtanphat.fragmentorientation;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -13,5 +19,29 @@ import java.util.List;
 public class NhanvienAdapter extends ArrayAdapter<Nhanvien> {
     public NhanvienAdapter(@NonNull Context context, int resource, @NonNull List<Nhanvien> objects) {
         super(context, resource, objects);
+    }
+    class ViewHolder{
+        TextView txtten;
+        ImageView imageView;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        ViewHolder viewHolder = null;
+        if (convertView == null){
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.dong_nhan_vien,null);
+            viewHolder = new ViewHolder();
+            viewHolder.txtten = convertView.findViewById(R.id.textviewten);
+            viewHolder.imageView = convertView.findViewById(R.id.imageviewlist);
+            convertView.setTag(viewHolder);
+        }else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+        Nhanvien nhanvien = getItem(position);
+        viewHolder.imageView.setImageResource(R.drawable.man);
+        viewHolder.txtten.setText(nhanvien.getTen());
+        return convertView;
     }
 }
